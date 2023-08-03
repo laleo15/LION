bbb12c8e94100eefa0e08cc7e128d28f2588bdb1
SonSuBin129 <ttokky0129@gmail.com>
2023-08-03 17:17:48 +0900

    mztest model 수정

--- project/oldmantest/models.py
+++ project/oldmantest/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 class TestQuestion(models.Model):
     subject = models.TextField()
-
+    GX=models.IntegerField() #~1980
+    GY=models.IntegerField() #1981~1996
+    GMZ=models.IntegerField() #1997~
     def __str__(self):
         return self.subject
 
-class Userdata(models.Model):
-    user=models.CharField(max_length=100)
+class Userdata(AbstractUser):
+    nickname=models.CharField(max_length=100)
+    generation=models.CharField(max_length=100)
+    
