d11df0e31c6a0134d1c8a68ac91fd26291148a85
SonSuBin129 <ttokky0129@gmail.com>
2023-08-05 13:23:53 +0900

    MZTest: TestResult까지 완성

--- project/oldmantest/models.py
+++ project/oldmantest/models.py
@@ -7,9 +7,16 @@ class TestQuestion(models.Model):
     left=models.TextField()
     right=models.TextField()
 
-    GX=models.IntegerField(default=0) #~1980
-    GY=models.IntegerField(default=0) #1981~1996
-    GMZ=models.IntegerField(default=0) #1997~
+    LGX=models.IntegerField(default=0) #~1980
+    RGX=models.IntegerField(default=0) #~1980
+
+    LGY=models.IntegerField(default=0) #1981~1996
+    RGY=models.IntegerField(default=0) #1981~1996
+
+    LGMZ=models.IntegerField(default=0) #1997~
+    RGMZ=models.IntegerField(default=0) #1997~
+
+    Total=models.IntegerField(default=0) #전체 값 저장할 변수
 
     def __str__(self):
         return self.contents
@@ -25,19 +32,6 @@ class MZUser(models.Model):
     #dictionary 일차원 -> key값에는 random 문제의 번호, value에는 문제 답 여부
     # value 값에 따른 상태
     # 0-> 해당 문제의 답을 아직 선택하지 않음.
-    # 0-> 해당 문제의 왼쪽 답을 선택
-    # 1-> 해당 문제의 오른쪽 답을 선택
-
-    #list 일차원 배열-> 0이면 해당 인덱스 번호 풀지 않은 것,
-    # 0이 아니면 해당 인덱스 번호를 푼 것.
     # 1-> 해당 문제의 왼쪽 답을 선택
     # 2-> 해당 문제의 오른쪽 답을 선택
-
-# class User(AbstractUser):
-#     #nickname=models.CharField(max_length=100)
-#     generation=models.CharField(max_length=100)
-#     questions=models.JSONField(default=[0]*100)
-
-#     def __str__(self):
-#         return self.username
 
--- project/oldmantest/views.py
+++ project/oldmantest/views.py
@@ -13,7 +13,6 @@ def question_setting():
         right=str(i)+"의 right"
 
         #이미 존재하는 문제는 삽입하지 않는다.
-
         try:
             Q=get_object_or_404(TestQuestion,contents=contents)
         except Http404:
@@ -23,8 +22,32 @@ def question_setting():
         
     return 0
 
+def Qsetting_update(user,UserDict):
+    question_list=TestQuestion.objects.all()    
+
+    for key, value in UserDict:
+        q=question_list[key]
+        if value==1:
+            if user.generation == "X세대":
+                q.LGX+=1
+            elif user.generation == "Y세대":
+                q.LGY+=1
+            elif user.generation == "MZ세대":
+                q.LGMZ+=1
+        if value==2:
+            print(user.generation)
+            if user.generation == "X세대":
+                q.RGX+=1
+            elif user.generation == "Y세대":
+                q.RGY+=1
+            elif user.generation == "MZ세대":
+                q.RGMZ+=1
+        q.Total=q.LGX+q.RGX+q.LGY+q.RGY+q.LGMZ+q.RGMZ
+        q.save()
+
 def login(request):
     question_setting()
+    
     return render(request,'oldmantest/login.html')
 
 def test(request):
@@ -66,50 +89,15 @@ def update_questions(request):
 
         user.questions=selected_Q
         user.save()
-        context={'user':user, 'dictionary':user.questions.items()}
+
+        Qsetting_update(user,user.questions.items())
+        QList=TestQuestion.objects.all()
+        
+        sendDict={}
+        for key in user.questions.keys():
+            sendDict[key]=[user.questions[key],QList[key]]
+        
+        context={'user':user, 'sendDict':sendDict.items()}
         
         return render(request,'oldmantest/testresult.html',context)
     return redirect('oldmantest:login')
-
-# def test(request):
-#     username=request.POST.get('username')
-#     generation=request.POST.get('generation')
-#     random_ten=random.sample(range(100),10) #100개문제에서 random하게 10개의 문제 추출
-    
-#     if generation=="none":
-#         generation="세대 선택을 하지 않았습니다"
-#     if username=="":
-#         username="닉네임을 작성하지 않았습니다"
-
-#     try:
-#         user = User.objects.get(username=username)
-#         user.questions={}
-#     except User.DoesNotExist:
-#         user=User(username=username, generation=generation,password=username)
-    
-#     #User가 있으면 받아오고, 없으면 새로 생성한다.
-#     #단 원래 사용자가 있던, 없던 문제는 접속할때마다 새로운 문제를 풀 수 있게 하기 위해서 random으로 계속 갱신해준다.
-
-#     for i in random_ten:
-#         user.questions[i]=0
-#     user.save()
-
-#     context={'user':user,'random_ten':random_ten}
-#     return render(request,'oldmantest/testpage.html',context)
-
-# def update_questions(request):
-#     if request.method=='POST':
-#         print("username:",request)
-#         user=request.user
-
-#         for key in user.questions.keys():
-#             selected=request.POST.get(f'group{key}')
-#             if selected == 1:
-#                 user.questions[key]=1
-#             elif selected == 2:
-#                 user.questions[key]=2
-        
-#         user.save()
-#         return render(request,'oldmantest/testresult.html')
-#     return redirect('oldmantest:login')
-
