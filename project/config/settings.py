439cf74561d8fb3770cd32250e72e98afd137e49
SonSuBin129 <ttokky0129@gmail.com>
2023-08-09 20:11:48 +0900

    allowed_hosts

--- project/config/settings.py
+++ project/config/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.219.32']
 
 #AUTH_USER_MODEL='oldmantest.User'
 
