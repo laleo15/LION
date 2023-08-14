fe10e5d91c5ee41ae744bffbc397709fc00f7170
laleo15 <jungleo411@gmail.com>
2023-08-15 00:49:37 +0900

    수정중

--- project/transmeme/urls.py
+++ project/transmeme/urls.py
@@ -7,5 +7,5 @@ app_name = 'transmeme'
 urlpatterns = [
     path('', views.main, name='translator'),
     path('translate/', views.translate, name='translate'),
-    #path('apitest/', views.translate1),
+    path('apitest/', views.translate1),
 ]
