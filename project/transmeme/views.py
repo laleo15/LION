3e8abcbef02f4840e4ff4971c13ca9c2f9e0e222
laleo15 <jungleo411@gmail.com>
2023-08-15 00:37:16 +0900

    주석 추가

--- project/transmeme/views.py
+++ project/transmeme/views.py
@@ -13,26 +13,26 @@ def main(request):
     return render(request, 'transmeme/translator.html')
         
 def translate(request):
-    word1 = Word.objects.all()
-    wordinput = request.POST.get(str('content'))
-    for w in word1:
-        if wordinput == w.subject:
-            synonym = Synonym.objects.filter(word=w)
-            example = Example.objects.filter(word=w)
+    word1 = Word.objects.all() #데이터베이스 가져오기
+    wordinput = request.POST.get(str('content')) #입력한 정보 가져오기 
+    for w in word1: # 모든 데이터에 접근할 수 있도록 반복문 
+        if wordinput == w.subject: #입력한 정보와 접근된 제이터와 같다면
+            synonym = Synonym.objects.filter(word=w) 
+            example = Example.objects.filter(word=w) #유의어와 예문 가져오기
             n = int(w.count)
-            Word.objects.filter(subject=w).update(count = n + 1)
-            lookup=Word.objects.all().order_by('-count')[:10]
+            Word.objects.filter(subject=w).update(count = n + 1) # 검색 1회 증가시킴
+            lookup=Word.objects.all().order_by('-count')[:10] # 검색횟수에 따른 딕셔너리 추가(순서정렬 후 앞에서부터 10개)
             context = {
                 "word": w,
                 "wordinput": wordinput,
                 'syno': synonym[0],
                 'ex': example[0],
                 "count": lookup
-            }
+            } #context에 잘 넣어서 html로 쏴줌 
             return render(request, 'transmeme/result.html', context)
 
-    # If no matching word is found, return this context
-    lookup=Word.objects.all().order_by('-count')[:10]
+    # If no matching word is found, return this context 
+    lookup=Word.objects.all().order_by('-count')[:10] #없을 경우에 대해서 동작 설명
     context = {
         "word": "잘못 입력하셨거나, 등록되지 않은 정보입니다. 다시 입력해 주세요",
         "wordinput": wordinput,
