55366ef7a2f34bcc346b3250fdbeae8258ec05cf
laleo15 <jungleo411@gmail.com>
2023-08-14 22:58:44 +0900

    transmeme 시리얼라이저 재작성 (테스트 전)

--- project/transmeme/views.py
+++ project/transmeme/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from dictionary.models import Word, Synonym, Example
+from .serializers import WordSerializer, SynonymSerializer, ExampleSerializer
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import WordSerializer, SynonymSerializer, ExampleSerializer
+
 
 
 # Create your views here.
@@ -40,26 +43,43 @@ def translate(request):
     return render(request, 'transmeme/result.html', context)
 
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Word, Synonym, Example
-from .serializers import WordSerializer, SynonymSerializer, ExampleSerializer
+def main(request):
+    return render(request, 'transmeme/transmeme.html')
 
+@csrf_exempt
+@api_view(['POST'])
+def translate(request):
+    if request.method == 'POST':
+        wordinput = request.POST.get('content')
+        try:
+            word = Word.objects.get(subject=wordinput)
+            synonym = Synonym.objects.filter(word=word)
+            example = Example.objects.filter(word=word)
+            n = int(word.count)
+            word.count = n + 1
+            word.save()
+
+            word_serializer = WordSerializer(word)
+            synonym_serializer = SynonymSerializer(synonym[0]) if synonym else None
+            example_serializer = ExampleSerializer(example[0]) if example else None
+
+            context = {
+                "word": word_serializer.data,
+                "syno": synonym_serializer.data if synonym_serializer else "해당 없음",
+                "ex": example_serializer.data if example_serializer else "해당 없음",
+            }
+            return render(request, 'transmeme/result.html', context)
+        except Word.DoesNotExist:
+            context = {
+                "word": "잘못 입력하셨거나, 등록되지 않은 정보입니다. 다시 입력해 주세요",
+                "syno": "해당 없음",
+                "ex": "해당 없음",
+            }
+            return render(request, 'transmeme/result.html', context)
+
+@csrf_exempt
 @api_view(['GET'])
-def get_word_detail(request, word_text):
-    word = Word.objects.get(subject=word_text)
-    word_serializer = WordSerializer(word)
-    
-    synonym = Synonym.objects.filter(word=word)
-    synonym_serializer = SynonymSerializer(synonym[0])
-    
-    example = Example.objects.filter(word=word)
-    example_serializer = ExampleSerializer(example[0])
-    
-    data = {
-        'word': word_serializer.data,
-        'synonym': synonym_serializer.data,
-        'example': example_serializer.data
-    }
-    
-    return Response(data)
+def get_rank(request):
+    words = Word.objects.all().order_by('-count')[:10]
+    word_list = [{"subject": word.subject} for word in words]
+    return Response(word_list)
