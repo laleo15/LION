8ad9d6b0460eed6670481a41c3b1721ff713baff
laleo15 <jungleo411@gmail.com>
2023-08-18 19:47:31 +0900

    db 수정

--- project/oldmantest/admin.py
+++ project/oldmantest/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import TestQuestion
+from .models import TestQuestion,Comment
 
 class QuestionAdmin(admin.ModelAdmin):
     search_fields=['subject']
 
 admin.site.register(TestQuestion, QuestionAdmin)
 #admin.site.register(User, UserAdmin)
+admin.site.register(Comment)
