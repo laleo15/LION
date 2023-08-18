320fcc728f0aa052494a2aedbcf012909107a429
laleo15 <jungleo411@gmail.com>
2023-08-18 20:01:46 +0900

    views.py 수정

--- project/transmeme/views.py
+++ project/transmeme/views.py
@@ -77,9 +77,9 @@ def translate_api(request):
             lookup = Word.objects.all().order_by('-count')[:10]
             context = {
                 "word": {
-                    "subject": "잘못 입력하셨거나, 등록되지 않은 정보입니다. 다시 입력해 주세요",
+                    "subject": "다시 입력해 주세요",
                     "meaning": "",
-                    "standard": "다시 입력해 주세요",
+                    "standard": "잘못 입력하셨거나, 등록되지 않은 정보입니다. 다시 입력해 주세요",
                     "count": 0,
                 },
                 "wordinput": wordinput,
@@ -106,72 +106,3 @@ def translate_api(request):
         "count": WordSerializer(lookup, many=True).data,
     }
     return Response(context)
-
-# def translate(request):
-#     word1 = Word.objects.all() #데이터베이스 가져오기
-#     wordinput = request.POST.get(str('content')) #입력한 정보 가져오기 
-#     for w in word1: # 모든 데이터에 접근할 수 있도록 반복문 
-#         if wordinput == w.subject: #입력한 정보와 접근된 제이터와 같다면
-#             synonym = Synonym.objects.filter(word=w) 
-#             example = Example.objects.filter(word=w) #유의어와 예문 가져오기
-#             n = int(w.count)
-#             Word.objects.filter(subject=w).update(count = n + 1) # 검색 1회 증가시킴
-#             lookup=Word.objects.all().order_by('-count')[:10] # 검색횟수에 따른 딕셔너리 추가(순서정렬 후 앞에서부터 10개)
-#             context = {
-#                 "word": w,
-#                 "wordinput": wordinput,
-#                 'syno': synonym[0],
-#                 'ex': example[0],
-#                 "count": lookup
-#             } #context에 잘 넣어서 html로 쏴줌 
-#             return render(request, 'transmeme/result.html', context)
-
-#     # If no matching word is found, return this context 
-#     lookup=Word.objects.all().order_by('-count')[:10] #없을 경우에 대해서 동작 설명
-#     context = {
-#         "word": "잘못 입력하셨거나, 등록되지 않은 정보입니다. 다시 입력해 주세요",
-#         "wordinput": wordinput,
-#         'syno': "해당 없음",
-#         'ex': '해당 없음',
-#         "count": lookup
-#     }
-#     return render(request, 'transmeme/result.html', context)
-
-
-# @api_view(['POST'])
-# def translate_api(request):
-#     data = request.data
-#     wordinput = data.get('content')
-#     print(wordinput)
-#     word_match = Word.objects.filter(subject=wordinput).first()
-#     print(word_match)
-#     if word_match:
-#         synonym = Synonym.objects.filter(word=word_match).first()
-#         example = Example.objects.filter(word=word_match).first()
-
-#         n = int(word_match.count)
-#         word_match.count = n + 1
-#         word_match.save()
-
-#         lookup = Word.objects.all().order_by('-count')[:10]
-        
-#         context = {
-#             "word": WordSerializer(word_match).data,
-#             "wordinput": wordinput,
-#             "syno": SynonymSerializer(synonym).data if synonym else "해당 없음",
-#             "ex": ExampleSerializer(example).data if example else "해당 없음",
-#             "count": WordSerializer(lookup, many=True).data,
-#         }
-#         print(context)
-#         return Response(context)
-
-#     lookup = Word.objects.all().order_by('-count')[:10]
-#     context = {
-#         "word": "잘못 입력하셨거나, 등록되지 않은 정보입니다. 다시 입력해 주세요",
-#         "wordinput": wordinput,
-#         'syno': "해당 없음",
-#         'ex': '해당 없음',
-#         "count": WordSerializer(lookup, many=True).data,
-#     }
-#     print(context)
-#     return Response(context)
